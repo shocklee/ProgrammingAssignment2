7c7ce167e03b5d2b4c4be47f4ae153ff2fa5eb26
Mark Shocklee <mark.j.shocklee@verizon.net>
2014-08-15 12:06:54 -0700

    Simple Changes
    Initial writing of the code simply changing what was provided by the
    instructors,

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Demonstrates the creation of a cache matrix and the computation of the
+## inverse of the matrix.
 
-## Write a short comment describing this function
+# This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        ## Create a matrix that can be cached.
+
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getatrix)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix has not changed), then the
+## cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
+
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
